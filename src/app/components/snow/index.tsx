45cc464577ce8ba8318f871ce86106ae8e0de3b7
@menyaboo <tosha.kolmay@mail.ru>
2024-05-12 19:07:50 +0700

    fix png mail

--- src/app/components/snow/index.tsx
+++ src/app/components/snow/index.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 const SnowSection = (): ReactNode => (
     <section className={styles.section} style={{backgroundImage: 'url(/images/usage/bg-section-4.png)'}}>
         <ContainerComponent className={styles.container}>
-            <Image src="/images/используемое/mail.png" alt="mail" width={285} height={182}/>
+            <Image src="/images/usage/mail.png" alt="mail" width={285} height={182}/>
             <div className={styles.description}>
                 <h3>Contact us</h3>
                 <p>You can email us at:
